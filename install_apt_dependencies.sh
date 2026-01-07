49f271405265aa3d1794bdcd9b690ac199ef80f3
Aaron Marburg <amarburg@uw.edu>
2026-01-07 05:47:04 +0000

    Load dependencies from script

---
+++ install_apt_dependencies.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+APT_DEPENDENCIES="cmake \
+                  g++ \
+                  libblas-dev \
+                  libboost-serialization-dev \
+                  libc++-dev \
+                  libegl1-mesa-dev \
+                  libepoxy-dev \
+                  libgl1-mesa-dev \
+                  libgles2-mesa-dev \
+                  libglew-dev \
+                  liblapack-dev \
+                  libopencv-dev \
+                  nasm \
+                  libwayland-dev \
+                  libx11-dev \
+                  libxkbcommon-dev \
+                  ninja-build \
+                  wayland-protocols"
+
+
+myarg=$1
+if [[ "$myarg" = "--deps" ]]; then
+    echo $APT_DEPENDENCIES
+    exit 0
+fi
+
+
+sudo apt-get update && \
+sudo apt-get install --no-install-recommends -y $APT_DEPENDENCIES
