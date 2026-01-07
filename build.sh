699692304e52eceb8a616fa18a73cea15d40598c
Aaron Marburg <amarburg@uw.edu>
2026-01-07 01:15:01 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,40 +1,13 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
+BUILD_TYPE=${BUILD_TYPE:-release}
 
-cd Thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
+git submodule sync vcpkg
+cd vcpkg && ./bootstrap-vcpkg.sh
+cd ..
 
-cd ../../g2o
+cmake --build --preset ninja-vcpkg-${BUILD_TYPE}
 
-echo "Configuring and building Thirdparty/g2o ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
-
-cd ../../Sophus
-
-echo "Configuring and building Thirdparty/Sophus ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
-
-cd ../../../
 
 echo "Uncompress vocabulary ..."
-
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
-
-echo "Configuring and building ORB_SLAM3 ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
