b38fab81af73acc53f96193c444d8b4d903217d2
Aaron Marburg <amarburg@uw.edu>
2026-01-07 21:09:24 +0000

    Don't explicitly install liblapack and liblas3

--- install_apt_dependencies.sh
+++ install_apt_dependencies.sh
@@ -7,7 +7,6 @@ APT_DEPENDENCIES="cmake \
                   libavfilter-dev \
                   libavformat-dev \
                   libavutil-dev \
-                  libblas3 \
                   libblas-dev \
                   libboost-serialization-dev \
                   libc++-dev \
@@ -18,7 +17,6 @@ APT_DEPENDENCIES="cmake \
                   libgl1-mesa-dev \
                   libgles2-mesa-dev \
                   libglew-dev \
-                  liblapack3 \
                   liblapack-dev \
                   libopencv-dev \
                   libspdlog-dev \
